483d315af2e6e3a45a00b6dacbc09b30d7fd2e72
Jacob Kanipe-Illig <jacobhyphenated@gmail.com>
2023-03-16 09:57:40 -0400

    fix link parsing text to use link role

--- src/util/linkParse.test.tsx
+++ src/util/linkParse.test.tsx
@@ -25,13 +25,13 @@ describe('Link Parse', () => {
       const text = 'this is a normal string with no links';
       const result = renderWithRouter(text);
       expect(result.getByText(text)).toBeDefined();
-      expect(result.queryAllByRole('button')).toHaveLength(0);
+      expect(result.queryAllByRole('link')).toHaveLength(0);
     });
 
     it('should handle a string with one link', () => {
       const text = 'we are linking to [[51]]one v. two[[/]] for this text';
       const result = renderWithRouter(text);
-      const link = result.getByRole('button');
+      const link = result.getByRole('link');
       expect(link.textContent).toBe('one v. two');
       expect(link).toHaveAttribute('href', '/case/51');
       expect(result.container.textContent).toBe('we are linking to one v. two for this text');
@@ -40,7 +40,7 @@ describe('Link Parse', () => {
     it('should handle a string with multiple links', () => {
       const text = 'we are linking to [[51]]one v. two[[/]] and also for [[6]]another case[[/]]';
       const result = renderWithRouter(text);
-      const links = result.getAllByRole('button');
+      const links = result.getAllByRole('link');
       expect(links).toHaveLength(2);
       expect(links[0].textContent).toBe('one v. two');
       expect(links[0]).toHaveAttribute('href', '/case/51');
@@ -53,14 +53,14 @@ describe('Link Parse', () => {
       const text = 'this is a [[bad]]bad case[[/]] link';
       const result = renderWithRouter(text);
       expect(result.getByText(text)).toBeDefined();
-      expect(result.queryAllByRole('button')).toHaveLength(0);
+      expect(result.queryAllByRole('link')).toHaveLength(0);
     });
 
     it('should handle a string with invalid link syntax', () => {
       const text = 'this is a [[6]]bad case[[/] link';
       const result = renderWithRouter(text);
       expect(result.getByText(text)).toBeDefined();
-      expect(result.queryAllByRole('button')).toHaveLength(0);
+      expect(result.queryAllByRole('link')).toHaveLength(0);
     });
   });
 
@@ -69,7 +69,7 @@ describe('Link Parse', () => {
       const text = 'we are linking to [[51]]one v. two[[/]] and also for [[6]]another case[[/]]';
       const result = render(stripLinks(text));
       expect(result.container.textContent).toBe('we are linking to one v. two and also for another case');
-      expect(result.queryAllByRole('button')).toHaveLength(0);
+      expect(result.queryAllByRole('link')).toHaveLength(0);
     });
   });
 });
